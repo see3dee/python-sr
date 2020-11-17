1845e543a7b058e8645e7d555b4dfc84cd0fd049
John Munsey <see3dee@gmail.com>
2020-11-17 12:03:21 -0800

    MIT icense from RealPython

--- filetotext.py
+++ filetotext.py
@@ -1,8 +1,8 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-harvard = sr.AudioFile('voicetest1.wav')
-with harvard as source:
+sample = sr.AudioFile('voicealphatest.wav')
+with sample as source:
     audio = r.record(source)
 type(audio)
 print(r.recognize_google(audio))
